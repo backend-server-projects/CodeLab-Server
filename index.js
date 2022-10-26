b47f68b70965297ba2b5c2471f6a5d0849c25b9a
Arman Khan <sparmankhan@gmail.com>
2022-10-26 09:27:25 +0600

    cources json file loaded

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
+const courses = require('./data/courses.json')
 
 
 app.use(cors())
@@ -11,6 +12,9 @@ app.get('/', (req, res)=>{
 })
 
 
+app.get('/courses', (req,res)=>{
+    res.send(courses)
+})
 
 app.listen(port, ()=>{
     console.log('Port running on', port);
